95e3d88a39df2c5da29aaac85812a52ee25574d1
ThanhLy1 <123609101+ThanhLy1@users.noreply.github.com>
2023-11-24 18:49:48 -1000

    Update Constants.js

--- app/imports/startup/server/utils/Constants.js
+++ app/imports/startup/server/utils/Constants.js
@@ -1,11 +1,11 @@
 // OpenAI related constants
 export const MAX_TOKENS_PER_ARTICLE = 4000;
 export const MAX_TOKENS_PER_MESSAGE = 2000;
-export const MAX_SESSION = 3;
 
 // Articles related constants
+export const MAX_SESSION = 3;
 export const MAX_ARTICLES = 4;
 export const MAX_SIMILAR_ARTICLES = 3;
 
-// AskUs related constants
+// Max token for embedding model ada_002
 export const MAX_TOKENS = 7500; // Max tokens allowed by the model: 8192
